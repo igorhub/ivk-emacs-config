a1fe49912a8643627d4654f0b71c5496c0678f39
Igor <igor.korobov.gh@gmail.com>
2020-06-17 18:13:14 +0300

    Fix a typo.

--- ivk-git.el
+++ ivk-git.el
@@ -8,7 +8,7 @@
     (kill-buffer ".gitdiff.diff"))
   (shell-command "git diff > /tmp/.gitdiff.diff")
   (find-file "/tmp/.gitdiff.diff")
-  (goto-char (min-point)))
+  (goto-char (point-min)))
 
 
 ;;; ivk-git.el ends here
