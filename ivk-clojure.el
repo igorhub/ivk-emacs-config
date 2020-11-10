ca879b08736143038d7f8c9ddb72964abc86f39e
Igor <igor.korobov.gh@gmail.com>
2020-11-10 18:02:36 +0300

    Add 'ivk.clojure/show-repl'.

--- ivk-clojure.el
+++ ivk-clojure.el
@@ -10,4 +10,13 @@
              (buffer-list))))
 
 
+(defun ivk.clojure/show-repl ()
+  "Split the window and open the CIDER REPL in the right pane."
+  (interactive)
+  (split-window-horizontally 100)
+  (other-window 1)
+  (switch-to-buffer (ivk.clojure/find-repl-buffer))
+  (other-window 1))
+
+
 ;;; ivk-clojure.el ends here
