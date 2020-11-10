d90814c80758c12a0f83eb24261a753d01a65712
Igor <igor.korobov.gh@gmail.com>
2020-11-10 18:02:26 +0300

    Remove 'ivk.clojure/repl' and 'ivk.clojure/unrepl'.

--- ivk-clojure.el
+++ ivk-clojure.el
@@ -10,16 +10,4 @@
              (buffer-list))))
 
 
-(defun ivk.clojure/repl ()
-  "Switch to REPL buffer."
-  (interactive)
-  (switch-to-buffer (ivk.clojure/find-repl-buffer)))
-
-
-(defun ivk.clojure/unrepl ()
-  "Switch from REPL buffer back to source."
-  (interactive)
-  (switch-to-buffer (other-buffer)))
-
-
 ;;; ivk-clojure.el ends here
