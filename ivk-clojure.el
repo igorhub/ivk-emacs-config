0a4c9474edc2eecc716cdbddcc3819db49f981ae
Igor <igor.korobov.gh@gmail.com>
2020-11-11 14:59:24 +0300

    Add REPL-window-related functions to 'ivk.clojure' namespace.

--- ivk-clojure.el
+++ ivk-clojure.el
@@ -10,6 +10,12 @@
              (buffer-list))))
 
 
+(defun ivk.clojure/find-repl-window ()
+  "Find the REPL window."
+  (let ((repl-buffer (ivk.clojure/find-repl-buffer)))
+    (when repl-buffer (get-buffer-window repl-buffer))))
+
+
 (defun ivk.clojure/show-repl ()
   "Split the window and open the CIDER REPL in the right pane."
   (interactive)
@@ -17,6 +23,29 @@
   (other-window 1)
   (switch-to-buffer (ivk.clojure/find-repl-buffer))
   (other-window 1))
+
+
+(defvar ivk.clojure/last-switch nil)
+
+(defun ivk.clojure/switch-to-or-out-of-repl-window ()
+  "Switch to (or out of) the CIDER REPL window."
+  (interactive)
+  (when (ivk.clojure/find-repl-window)
+    (let ((switch-out-of-repl? (or (eq major-mode 'cider-repl-mode)
+                                   (and (eq major-mode 'fundamental-mode)
+                                        ivk.clojure/last-switch))))
+      (if switch-out-of-repl?
+          (evil-window-left 1)
+        (evil-window-right 1))
+      (setq ivk.clojure/last-switch (not switch-out-of-repl?)))))
+
+
+(defun ivk.clojure/open-and-switch-to-or-out-of-repl-window ()
+  "Switch to (or out of) the CIDER REPL window."
+  (interactive)
+  (if (not (ivk.clojure/find-repl-window))
+      (ivk.clojure/show-repl)
+    (ivk.clojure/switch-to-or-out-of-repl-window)))
 
 
 ;;; ivk-clojure.el ends here
