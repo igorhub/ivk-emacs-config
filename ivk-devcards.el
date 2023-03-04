f7c1ca4b01092a669423e6c64935f74e0b0c9947
Igor <igor.korobov.gh@gmail.com>
2023-03-04 17:25:19 +0300

    Add devcards package

---
+++ ivk-devcards.el
@@ -0,0 +1,31 @@
+(provide 'ivk-devcards)
+
+(require 'request)
+
+
+(defun ivk.devcards/current-package ()
+  "Return the devcard at point."
+  (save-excursion
+    (search-backward-regexp "^package +\\([a-zA-Z0-9_]+\\)")
+    (buffer-substring-no-properties (match-beginning 1) (match-end 1))))
+
+
+(defun ivk.devcards/devcard-under-cursor ()
+  "Return the devcard at point."
+  (save-excursion
+    (let ((package (ivk.devcards/current-package)))
+      (search-backward-regexp "func +\\([a-zA-Z0-9_]+\\).*\\*devcards.Devcard) {")
+      (concat package "." (buffer-substring-no-properties (match-beginning 1) (match-end 1))))))
+
+
+(defun ivk.devcards/set-devcard-under-cursor ()
+  (interactive)
+  (request "http://localhost:50050/api/set-devcards"
+    :type "POST"
+    :data (json-encode `(("devcards" . [,(ivk.devcards/devcard-under-cursor)])))
+    :success (cl-function
+              (lambda (&key data &allow-other-keys)
+                (ivk/refresh-emacshelper)))))
+
+
+;;; ivk-devcards.el ends here
