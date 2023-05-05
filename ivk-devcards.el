e005bf58a9ba2dc3cff424bebf1b06cf407f32f0
Igor <igor.korobov.gh@gmail.com>
2023-05-05 22:05:48 +0300

    Set project in ivk.devcards/set-devcard-under-cursor

--- ivk-devcards.el
+++ ivk-devcards.el
@@ -10,6 +10,17 @@
     (buffer-substring-no-properties (match-beginning 1) (match-end 1))))
 
 
+(defun ivk.devcards/current-project (path)
+  "Return the Golang project for the current buffer."
+  (let* ((parent (file-name-directory (directory-file-name path))))
+    (if (or (file-exists-p (concat path "go.mod"))
+            (file-exists-p (concat path "project.json")))
+        path
+      (if (not (equalp parent "/"))
+          (ivk.devcards/current-project parent)
+        nil))))
+
+
 (defun ivk.devcards/devcard-under-cursor ()
   "Return the devcard at point."
   (save-excursion
@@ -18,14 +29,31 @@
       (concat package "." (buffer-substring-no-properties (match-beginning 1) (match-end 1))))))
 
 
-(defun ivk.devcards/set-devcard-under-cursor ()
-  (interactive)
-  (request "http://localhost:50050/api/set-devcards"
+(defun ivk.devcards/set-project (project)
+  (request "http://localhost:50050/api/set-project"
     :type "POST"
-    :data (json-encode `(("devcards" . [,(ivk.devcards/devcard-under-cursor)])))
+    :data (json-encode `(("project" . ,project)))
     :success (cl-function
               (lambda (&key data &allow-other-keys)
                 (ivk/refresh-emacshelper)))))
+
+
+(defun ivk.devcards/set-devcard (card)
+  (request "http://localhost:50050/api/set-devcards"
+    :type "POST"
+    :data (json-encode `(("devcards" . [,card])))
+    :success (cl-function
+              (lambda (&key data &allow-other-keys)
+                (ivk/refresh-emacshelper)))))
+
+
+(defun ivk.devcards/set-devcard-under-cursor ()
+  (interactive)
+  (let ((project (ivk.devcards/current-project (buffer-file-name)))
+        (card (ivk.devcards/devcard-under-cursor)))
+    (message (format "prj: %s; card: %s" project card))
+    (ivk.devcards/set-project project)
+    (ivk.devcards/set-devcard card)))
 
 
 ;;; ivk-devcards.el ends here
