6cb02e922f445db4e747a55471aef5e20453b75d
Igor <igor.korobov.gh@gmail.com>
2024-05-07 15:02:55 +0300

    Add ivk/open-in-vscode

--- ivk-os-integration.el
+++ ivk-os-integration.el
@@ -6,4 +6,12 @@
   (start-process "ivk/play" nil "remote-play.bb.clj" (buffer-file-name)))
 
 
+(defun ivk/open-in-vscode ()
+  "Open the file in vscode."
+  (interactive)
+  (start-process "whatever" nil
+                 "code" "-g" (concat (buffer-file-name) ":" (format-mode-line "%l"))))
+
+
+
 ;;; ivk-os-integration.el ends here
